4b5b6348dda56fd872b9cbeffaceb6a0336dfac1
Raphael Boidol <boidolr@users.noreply.github.com>
2024-11-23 12:02:45 +0100

    feat: switch to astro

--- boah.md
+++
@@ -1,8 +0,0 @@
----
-layout: page
-title: Boah
-permalink: /boah/
----
-
-Sehr beeindruckend! Sprich das Brautpaar auf `Fazer` an und sichere dir deine Belohnung.
-
---
+++ src/navigation.ts
@@ -0,0 +1,45 @@
+import { toHtmlId } from '~/utils/utils';
+import { getPermalink } from '~/utils/permalinks';
+
+export const headerData = {
+  links: [
+    {
+      text: 'Navigation',
+      links: [
+        {
+          text: 'Wann und Wo',
+          href: getPermalink('/#' + toHtmlId('Wann und Wo')),
+        },
+        {
+          text: 'RSVP',
+          href: getPermalink('/#' + toHtmlId('Rückmeldung')),
+        },
+        {
+          text: 'Unterkunft vor Ort',
+          href: getPermalink('/#' + toHtmlId('Unterkunft vor Ort')),
+        },
+        {
+          text: 'Anreise',
+          href: getPermalink('/#' + toHtmlId('Anreise')),
+        },
+        {
+          text: 'Über die Gegend',
+          href: getPermalink('activities'),
+        },
+        {
+          text: 'Geschenke',
+          href: getPermalink('/#' + toHtmlId('Geschenke')),
+        },
+        {
+          text: 'Kontakt',
+          href: getPermalink('/#' + toHtmlId('Kontaktadressen')),
+        },
+      ],
+    },
+  ],
+  actions: [],
+};
+
+export const footerData = {
+  footNote: `<a href="/codenames">©</a>&nbsp;2024 Kirstin & Jonathan&nbsp;|&nbsp;<a class="underline" href="mailto:info@kirstin-und-jonathan.de">Kontakt</a>`,
+};
