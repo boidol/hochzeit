a7ed5453c6c365c8af3962e9117559f72643bd6f
Raphael Boidol <boidolr@users.noreply.github.com>
2024-11-24 10:42:23 +0100

    Exclude pages from sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -25,7 +25,9 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
-    sitemap(),
+    sitemap({
+      filter: (page) => !(page.includes('boah') || page.includes('codenames')),
+    }),
     icon({
       include: {
         tabler: ['*'],
